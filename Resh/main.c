5594aba1df029975e1ee97bdf9f618cac18e8efc
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-09-14 21:01:40 +0530

    Update main.c

--- Resh/main.c
+++ Resh/main.c
@@ -1,6 +1,3 @@
-// Code by Aryan Basnet
-//07-09-2024
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -12,15 +9,15 @@
 #include "hal/gpio_types.h"
 #include "portmacro.h"
 #include "soc/gpio_num.h"
+#include "esp_log.h" 
 
-//
+#define TAG "Motor_Control"  // Define a tag for logging
 
-#define go_left_forward      34
+
+#define go_left_forward      18  // Changed to D18
 #define go_left_backward     35
-#define go_right_forward     36
+#define go_right_forward     19  // Changed to D19
 #define go_right_backward    39
-
-//yet to be defined
 
 #define ENA   32
 #define IN1   33
@@ -30,105 +27,88 @@
 #define IN3   27
 #define IN4   14
 
-
 void control_task_left(void *arg)
 {
-  while(1)
-  {
-	//gpio value holders
-     
-    int gpio_35 = gpio_get_level(35);
-    int gpio_34 = gpio_get_level(34);
-        
-   
-    
-	if(gpio_34 == 1) //left forward
-	{
-	   gpio_set_level(ENA,1);
-	   gpio_set_level(IN1,1);
-	   gpio_set_level(IN2,0);
-	   
-	   
-	}
-	else if(gpio_35 == 1) //left backward
+    while(1)
     {
-	   gpio_set_level(ENA,1);
-	   gpio_set_level(IN1,0);
-	   gpio_set_level(IN2,1);
-	  
-	}
-	else
-	{
-	   gpio_set_level(ENA,0);
-	   gpio_set_level(IN1,0);
-	   gpio_set_level(IN2,0);
-	}
-	
-	vTaskDelay(25/portTICK_PERIOD_MS);
+      
+        int gpio_35 = gpio_get_level(go_left_backward);
+        int gpio_18 = gpio_get_level(go_left_forward);
 
- }
+        ESP_LOGI(TAG, "Left Forward GPIO[%d]: %d, Left Backward GPIO[%d]: %d", go_left_forward, gpio_18, go_left_backward, gpio_35);
+
+        if(gpio_18 == 1)  // left forward
+        {
+            gpio_set_level(ENA, 1);
+            gpio_set_level(IN1, 1);
+            gpio_set_level(IN2, 0);
+            ESP_LOGI(TAG, "Left motor moving forward");
+        }
+        else  // Stop
+        {
+            gpio_set_level(ENA, 0);
+            gpio_set_level(IN1, 0);
+            gpio_set_level(IN2, 0);
+            ESP_LOGI(TAG, "Left motor stopped");
+        }
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
 }
 
 void control_task_right(void *arg)
 {
-	//gpio value holders
- while(1)
- {
-     int gpio_36 = gpio_get_level(36);
-     int gpio_39 = gpio_get_level(39);
-      
-    
-	if(gpio_36 == 1 ) //right forward
-	{
-	   gpio_set_level(ENB,1);
-	   gpio_set_level(IN3,1);
-	   gpio_set_level(IN4,0);
-	   
-	}
-	else if(gpio_39 == 1) //right backward
+    while(1)
     {
-	    gpio_set_level(ENB,1);
-	    gpio_set_level(IN3,0);
-	    gpio_set_level(IN4,1);
-	}
-	else 
-	{
-		gpio_set_level(ENB,0);
-	    gpio_set_level(IN3,0);
-	    gpio_set_level(IN4,0);
-	}
-	
-	vTaskDelay(25/portTICK_PERIOD_MS);
+       
+        int gpio_19 = gpio_get_level(go_right_forward);
+        int gpio_39 = gpio_get_level(go_right_backward);
 
+        ESP_LOGI(TAG, "Right Forward GPIO[%d]: %d, Right Backward GPIO[%d]: %d", go_right_forward, gpio_19, go_right_backward, gpio_39);
+
+        if(gpio_19 == 1)  // right forward
+        {
+            gpio_set_level(ENB, 1);
+            gpio_set_level(IN3, 1);
+            gpio_set_level(IN4, 0);
+            ESP_LOGI(TAG, "Right motor moving forward");
+        }
+        else  // Stop
+        {
+            gpio_set_level(ENB, 0);
+            gpio_set_level(IN3, 0);
+            gpio_set_level(IN4, 0);
+            ESP_LOGI(TAG, "Right motor stopped");
+        }
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
 }
-}
-   
-
-
 
 void app_main(void)
 {
-	//gpio config start
-	//gpio config
-	gpio_set_direction(35, GPIO_MODE_INPUT);
-    gpio_set_direction(36, GPIO_MODE_INPUT);
-    gpio_set_direction(39, GPIO_MODE_INPUT);
-    gpio_set_direction(34, GPIO_MODE_INPUT);
-    
    
-    //output gpio config
-    gpio_config_t io_conf;
-    io_conf.intr_type= GPIO_INTR_DISABLE;
-    io_conf.mode=GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask =  (1ULL << GPIO_NUM_32) | (1ULL << GPIO_NUM_33) | (1ULL << GPIO_NUM_25) | (1ULL << GPIO_NUM_26) | (1ULL << GPIO_NUM_27) | (1ULL << GPIO_NUM_14); //more to be defined
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    gpio_config(&io_conf);
-    
-   
-    
-    xTaskCreatePinnedToCore(&control_task_left,"control task left" , 4092, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(&control_task_right,"control task right" , 4092, NULL, 5, NULL, 1);
-  
+    gpio_config_t input_conf;
+    input_conf.intr_type = GPIO_INTR_DISABLE;
+    input_conf.mode = GPIO_MODE_INPUT;
+    input_conf.pin_bit_mask = (1ULL << go_left_backward) | (1ULL << go_left_forward) |
+                              (1ULL << go_right_backward) | (1ULL << go_right_forward);
+    input_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    input_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&input_conf);
 
+    gpio_config_t output_conf;
+    output_conf.intr_type = GPIO_INTR_DISABLE;
+    output_conf.mode = GPIO_MODE_OUTPUT;
+    output_conf.pin_bit_mask = (1ULL << ENA) | (1ULL << IN1) | (1ULL << IN2) |
+                               (1ULL << ENB) | (1ULL << IN3) | (1ULL << IN4);
+    output_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    output_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    gpio_config(&output_conf);
+    
+    ESP_LOGI(TAG, "GPIOs configured for motor control");
+
+    // Create tasks for left and right motor control
+    xTaskCreatePinnedToCore(&control_task_left, "control task left", 4096, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(&control_task_right, "control task right", 4096, NULL, 5, NULL, 1);
 }
