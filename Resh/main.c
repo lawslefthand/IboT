6fccd4d5973319f7d40a139b9bf984c17e68ccfa
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2025-02-13 23:24:35 +0530

    Update main.c

--- Resh/main.c
+++ Resh/main.c
@@ -11,39 +11,49 @@
 #include "soc/gpio_num.h"
 #include "esp_log.h" 
 
-#define TAG "Motor_Control"  // Define a tag for logging
+QueueHandle_t xQueue;
 
+char uart_read()
+{
+	    // Configure a temporary buffer for the incoming data                                                                                                                
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
 
-#define go_left_forward      18  // Changed to D18
-#define go_left_backward     35
-#define go_right_forward     19  // Changed to D19
-#define go_right_backward    39
+    while (1) {
+        // Read data from the UART
+        char data=[8];
+        data = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        
+        return data;
+    }
+}
+}
 
-#define ENA   32
-#define IN1   33
-#define IN2   25
-
-#define ENB   26
-#define IN3   27
-#define IN4   14
 
 void control_task_left(void *arg)
 {
     while(1)
     {
       
-        int gpio_35 = gpio_get_level(go_left_backward);
-        int gpio_18 = gpio_get_level(go_left_forward);
+      data_left=[8];
+      
+      data_left=uart_read();
 
         ESP_LOGI(TAG, "Left Forward GPIO[%d]: %d, Left Backward GPIO[%d]: %d", go_left_forward, gpio_18, go_left_backward, gpio_35);
 
-        if(gpio_18 == 1)  // left forward
+        if()  // left forward
         {
             gpio_set_level(ENA, 1);
             gpio_set_level(IN1, 1);
             gpio_set_level(IN2, 0);
             ESP_LOGI(TAG, "Left motor moving forward");
         }
+        else if ()
+        {
+			gpio_set_level(ENA, 1);
+            gpio_set_level(IN1, 0);
+            gpio_set_level(IN2, 1);
+            ESP_LOGI(TAG, "Left motor moving backwards");
+		}
         else  // Stop
         {
             gpio_set_level(ENA, 0);
@@ -52,7 +62,7 @@ void control_task_left(void *arg)
             ESP_LOGI(TAG, "Left motor stopped");
         }
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(200/ portTICK_PERIOD_MS);
     }
 }
 
@@ -65,14 +75,25 @@ void control_task_right(void *arg)
         int gpio_39 = gpio_get_level(go_right_backward);
 
         ESP_LOGI(TAG, "Right Forward GPIO[%d]: %d, Right Backward GPIO[%d]: %d", go_right_forward, gpio_19, go_right_backward, gpio_39);
+  
+         data_left=[8];
+      
+         data_left=uart_read();
 
-        if(gpio_19 == 1)  // right forward
+        if()  // right forward
         {
             gpio_set_level(ENB, 1);
             gpio_set_level(IN3, 1);
             gpio_set_level(IN4, 0);
             ESP_LOGI(TAG, "Right motor moving forward");
         }
+        else if ()
+        {
+			gpio_set_level(ENB, 1);
+            gpio_set_level(IN3, 0);
+            gpio_set_level(IN4, 1);
+            ESP_LOGI(TAG, "Right motor moving backward");
+		}
         else  // Stop
         {
             gpio_set_level(ENB, 0);
@@ -81,21 +102,14 @@ void control_task_right(void *arg)
             ESP_LOGI(TAG, "Right motor stopped");
         }
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(2-0 / portTICK_PERIOD_MS);
     }
 }
 
 void app_main(void)
 {
    
-    gpio_config_t input_conf;
-    input_conf.intr_type = GPIO_INTR_DISABLE;
-    input_conf.mode = GPIO_MODE_INPUT;
-    input_conf.pin_bit_mask = (1ULL << go_left_backward) | (1ULL << go_left_forward) |
-                              (1ULL << go_right_backward) | (1ULL << go_right_forward);
-    input_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    input_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    gpio_config(&input_conf);
+
 
     gpio_config_t output_conf;
     output_conf.intr_type = GPIO_INTR_DISABLE;
@@ -106,8 +120,27 @@ void app_main(void)
     output_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     gpio_config(&output_conf);
     
+      * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = ECHO_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    int intr_alloc_flags = 0;
+    
+     ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+
+
+
+    
     ESP_LOGI(TAG, "GPIOs configured for motor control");
 
+     xQueue = xQueueCreate(5, sizeof(char));
     // Create tasks for left and right motor control
     xTaskCreatePinnedToCore(&control_task_left, "control task left", 4096, NULL, 5, NULL, 0);
     xTaskCreatePinnedToCore(&control_task_right, "control task right", 4096, NULL, 5, NULL, 1);
