7eb9ce22d02fe8ca508fa131f86a3091dc5635b2
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2025-02-15 23:32:10 +0530

    Update main.c

--- Resh/main.c
+++ Resh/main.c
@@ -1,6 +1,7 @@
-\#include <stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/idf_additions.h"
 #include "freertos/task.h"
@@ -19,105 +20,104 @@
 #define ENB   26
 #define IN3   27
 #define IN4   14
+
+#define TXD_PIN 17
+#define RXD_PIN 16
+#define BUF_SIZE 1024
     
+static const char* TAG = "UART";
 
-#define BUF_SIZE (1024)
 
-QueueHandle_t xQueue;
 
-char uart_read()
+void control_task(void *arg)
 {
-	   
-
-    while (1) {
-        // Read data from the UART
-       
-        char data;
-        uart_read_bytes(0, &data, (BUF_SIZE)-1,  20 / portTICK_PERIOD_MS);
-        
-        return data; 
-    }
-}
-
-
-
-void control_task_left(void *arg)
-{
-    while(1)
+   
+		 uint8_t data[BUF_SIZE];
+    while (1)
     {
+        int len = uart_read_bytes(UART_NUM_1, data, BUF_SIZE, 100 / portTICK_PERIOD_MS);
+        if (len > 0)
+        {
+            data[len] = '\0'; // Null-terminate string
+            ESP_LOGI(TAG, "Received: %s", data);
+           // uart_write_bytes(UART_NUM_1, (const char *)data, len); // Echo back
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
       
-      char data_left;
-      
-      data_left=uart_read();
-
-       
-        if(data_left == 'L')  // left forward
+        if(data[0] == 'F')  // left forward
         {
             gpio_set_level(ENA, 1);
             gpio_set_level(IN1, 1);
             gpio_set_level(IN2, 0);
-            printf("left motor moving forwards");
+            printf("left motor moving forwards\n");
+             ESP_LOGI(TAG,"Left motor going forwards\n");
+            
         }
-        else if (data_left == 'B')
+        else if (data[0] == 'B')
         {
 			gpio_set_level(ENA, 1);
             gpio_set_level(IN1, 0);
             gpio_set_level(IN2, 1);
            printf("left motor moving backwardss");
+           ESP_LOGI(TAG,"Left motor going backwards\n");
+            
 		}
-        else  // Stop
-        {
-            gpio_set_level(ENA, 0);
-            gpio_set_level(IN1, 0);
-            gpio_set_level(IN2, 0);
-            printf("left motor stopped");
-        }
-
-        vTaskDelay(200/ portTICK_PERIOD_MS);
-    }
-}
-
-void control_task_right(void *arg)
-{
-    while(1)
-    {
-       
-         char data_right=0;
-      
-         data_right=uart_read();
-
-        if(data_right == 'R')  // right forward
+        else if(data[0]== 'T')  
         {
             gpio_set_level(ENB, 1);
             gpio_set_level(IN3, 1);
             gpio_set_level(IN4, 0);
-            printf("Right motor going forward");
+            printf("Right motor going forward\n");
+            ESP_LOGI(TAG,"Right motor going forward\n");
         }
-        else if (data_right == 'B')
+        else if (data[0] == 'X')
         {
 			gpio_set_level(ENB, 1);
             gpio_set_level(IN3, 0);
             gpio_set_level(IN4, 1);
-            printf("Right motor going backwards");
+            printf("Right motor going backwards\n");
+            ESP_LOGI(TAG,"Right motor going Backwards\n");
+            
          }
+         else if (data[0] == 'A')
+         {
+			    gpio_set_level(ENA, 1);
+            gpio_set_level(IN1, 1);
+            gpio_set_level(IN2, 0);
+            printf("left motor moving forwards\n");
+             ESP_LOGI(TAG,"Left motor going forwards\n");
+              gpio_set_level(ENB, 1);
+            gpio_set_level(IN3, 1);
+            gpio_set_level(IN4, 0);
+            printf("Right motor going forward\n");
+            ESP_LOGI(TAG,"Right motor going forward\n");
+             
+		 }
            
         else  // Stop
         {
             gpio_set_level(ENB, 0);
             gpio_set_level(IN3, 0);
             gpio_set_level(IN4, 0);
-            printf("Both motors stopped");
+            gpio_set_level(ENA, 0);
+            gpio_set_level(IN1, 0);
+            gpio_set_level(IN2, 0);
+            printf("left motor stopped\n");
+           ESP_LOGI(TAG,"Left motor stopped\n");
+            printf("Right motors stopped\n");
+             ESP_LOGI(TAG,"Right motor stopped\n");
+            
            
         }
 
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+        vTaskDelay(200/ portTICK_PERIOD_MS);
     }
 }
 
+
+
 void app_main(void)
 {
-   
-
 
     gpio_config_t output_conf;
     output_conf.intr_type = GPIO_INTR_DISABLE;
@@ -130,20 +130,17 @@ void app_main(void)
     
       
     uart_config_t uart_config = {
-        .baud_rate = 115200,
+        .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
+        .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
     };
-    int intr_alloc_flags = 0;
-    
-   uart_driver_install(0, BUF_SIZE*2, 0, 0, NULL, intr_alloc_flags);
-   uart_param_config(0, &uart_config);
-   uart_set_pin(0, 1, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(UART_NUM_1, BUF_SIZE, 0, 0, NULL, 0);
    
     // Create tasks for left and right motor control
-    xTaskCreatePinnedToCore(&control_task_left, "control task left", 4096, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(&control_task_right, "control task right", 4096, NULL, 5, NULL, 1);
+   xTaskCreatePinnedToCore(&control_task, "control task", 4096, NULL, 5, NULL, 1);
 }
